8baa121d546f7186133da84fdbbf0fc5b02e41d9
Veikko Lehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-11-22 16:42:49 +0200

    Update test command for coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleDirectories: ['node_modules', 'src'],
-  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/index.tsx'],
+  collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/index.jsx'],
   setupTestFrameworkScriptFile: '<rootDir>/src/setupTests.js',
 };
